150e95dd28b12b8436d6bbd971300b8331fc0e45
kibablu <kibablu16@gmail.com>
2020-04-02 07:51:51 +0300

    password.py
    added comments

--- password.py
+++ password.py
@@ -8,11 +8,11 @@ PASSWORDS = {
     'luggage': '12345'
 }
 
-if len(sys.argv) < 2:
-    print('Usage: python pw.py [account] - copy account password')
+if len(sys.argv) < 2: # checks if sys.argv is less that 2 from a command 
+    print('Usage: python password.py [account] - copy account password')
     sys.exit()
 
-account = sys.argv[1] # first command line arg is the account name -- list argument from the PASSWORD Dictionary
+account = sys.argv[1] # first command line arg is the account name -- sys.argv is a list argument from a command
 
 if account in PASSWORDS: # check if the arguments are in PASSWORDS Dictionary
     pyperclip.copy(PASSWORDS[account])
@@ -20,5 +20,12 @@ if account in PASSWORDS: # check if the arguments are in PASSWORDS Dictionary
 else:
     print('There is no account named ' + account)
 
+'''
+This new code looks in the PASSWORDS dictionary for the account name.
+If the account name is a key in the dictionary, we get the value corresponding
+to that key, copy it to the clipboard, and print a message saying that we
+copied the value. Otherwise, we print a message saying there’s no account
+with that name.
+'''
 
 # output after running python password.py email -- example@gmail.com
